b3cd4bb87e514aaa1eb5b89966a4486d2b22e177
Hosffman Meza <hosffmanmeza@hotmail.com>
2016-05-28 17:33:31 -0600

    Add files via upload

---
+++ ExamenFinal_Calculadora/calculadora.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void cientifica(void);
+void binaria(void);
+void estandar(void);
+void estadistica(void);
+int main(void){
+	
+}
+
+void cientifica(){
+	
+}
+
+void binaria(){
+	
+}
+
+void estandar(){
+	
+}
+
+void estadistica(){
+	
+}
